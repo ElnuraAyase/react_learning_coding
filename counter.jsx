7b61277f55eb489d3333c1165f1f5c8451acfc6b
Elnura <elya.ayase@gmail.com>
2024-03-08 22:54:33 +0400

    Update counter.jsx

--- counter.jsx
+++ counter.jsx
@@ -1,1 +1,23 @@
+import React, { useState } from react';
+
+const Counter = () = {
+  // State variable to store the counter value
+  const [count setCount] = useState(0);
+
+  // Function to handle incrementing the counter
+  const handleIncrement = () => {
+    setCount(count + 1);    // Increment the count by 1
+  };
+
+  return (
+    <div>
+      {/* Display the current count */}
+      <p>Count: {count}</p>
+      {/* Button to increment the counter */}
+      <button onClick=  {handleIncrement}>Increment</button>
+    <div>
+  );
+};
+
+export default Counter;
 

7b798d216688f15c71187fb4b391027c39eaa2ba
Elnura <elya.ayase@gmail.com>
2024-03-08 22:48:02 +0400

    Update guessingGame.jsx

--- guessingGame.jsx
+++ guessingGame.jsx
@@ -1,1 +1,11 @@
 
+import React, { useState } from 'react';
+
+const GuessingGame = () => {
+  const [number, setNumber] = useState(Math.floor(Math.random() * 100) + 1);
+  const [guess, setGuess] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleInputChange = (event) => {
+    setGuess(event.target.value);
+  };

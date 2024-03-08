afe3e71a0866f0ba2dee5327766849199cfc9bd7
Elnura <elya.ayase@gmail.com>
2024-03-08 22:52:52 +0400

    Update clickME.jsx

--- clickME.jsx
+++ clickME.jsx
@@ -2,9 +2,13 @@
 import React, { useState } from 'react';
 
 const ButtonToggle = () => {
+    // Define state variable to track if the button is clicked or not
   const [isClicked, setIsClicked] = useState(false);
 
+    // Function to handle button click event
   const handleClick = () => {
+        // Toggle the value of isClicked state when button is clicked
+
     setIsClicked(!isClicked);
   };
   return (

0f741093037b0c7ce984a393a8c1ddb54e015f43
Elnura <elya.ayase@gmail.com>
2024-03-08 22:50:21 +0400

    Update clickME.jsx

--- clickME.jsx
+++ clickME.jsx
@@ -1,1 +1,9 @@
 
+import React, { useState } from 'react';
+
+const ButtonToggle = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };

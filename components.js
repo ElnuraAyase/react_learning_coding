26f04b40c251a0c25f45bc718802c7cb7e45af0d
Elnura <elya.ayase@gmail.com>
2024-03-19 22:38:01 +0400

    Update components.js

--- components.js
+++ components.js
@@ -1,1 +1,18 @@
+import React from 'react'; // Import React package
 
+class SimpleComponent extends React.Component { // Define a class component
+  constructor(props) {
+    super(props);
+    // Initialize state
+    this.state = {
+      count: 0 // Initial count value
+    };
+  }
+
+  // Method to increment count
+  incrementCount = () => {
+    // Update state to increment count by 1
+    this.setState({ count: this.state.count + 1 });
+  }
+
+ 
